2680e31c5c327c79324eee8d482e00c687c2a950
Ethan Niser <ethan@niser.com>
2024-03-07 19:48:23 +0000

    fix snippet loading error

--- nvim/lua/kickstart/plugins/cmp.lua
+++ nvim/lua/kickstart/plugins/cmp.lua
@@ -35,6 +35,7 @@ return {
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup {
         snippet = {
